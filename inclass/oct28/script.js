df595cd8c4bd81ba159e9de7dbf7bd938aaf1ea7
Jahred Danker <jahrede@gmail.com>
2019-10-28 15:24:23 -0400

    added shoes

--- inclass/oct28/script.js
+++ inclass/oct28/script.js
@@ -1,16 +1,32 @@
-async function special(){
-    console.log("Beginning of special")
-    let result = await setTimeout(doStuff, 2000)
-    console.log("Ending of special")
+async function displayShoe() {
+    let response = await fetch("https://jdanker.github.io/inclass/oct28/shoe.json")
+    let shoeJson = await response.json();
 
-}
+    let shoeName = shoeJson.name;
+    let shoeBrand = shoeJson.brand
+    let reviews = shoeJson.comments
+    
+    console.log(`${shoeName} by ${shoeBrand}`)
 
-function doStuff() {
-    console.log("In do stuff!")
+    for(i in reviews) {
+        let comments = document.createElement("p")
+
+        comments.innerHTML = reviews[i]
+    }
+
+    let section = document.createElement("section")
+    section.innerHTML = `${shoeName} by ${shoeBrand}`
+    let content =  document.getElementById("content")
+    content.append(section)
+
+
 }
 
 window.onload = function() {
-    this.console.log("Before Calling Special")
-    this.special()
-    this.console.log("After calling special")
+    this.displayShoe()
+    let content = this.document.getElementById("content")
+    let h1 = document.createElement("h1")
+    content.append(h1)
+    h1.innerHTML= "I love shoes"
+
 }
