be9d79bbf04b42050a26dfdd9d3189b34d425be4
Jahred Danker <jahrede@gmail.com>
2019-11-06 15:37:52 -0500

    fin nov 06

--- inclass/nov06/public/script.js
+++ inclass/nov06/public/script.js
@@ -1,7 +1,27 @@
 async function showCakes(){
     let response = await fetch('/api/cakes')
     let cakesJSON = await response.json()
-    console.log(cakesJSON)
+
+    let contentDiv = document.getElementById('content')
+
+    for(i in cakesJSON){
+        contentDiv.append(makeCake(cakesJSON[i]))
+    }
+    
+}
+
+function makeCake(cake){
+    const cakeElem = document.createElement('div')
+    cakeElem.classList.add('cake')
+    cakeH2 = document.createElement('h2')
+    cakeH2.innerHTML = cake.name
+    cakeElem.append(cakeH2)
+    cakeP = document.createElement('p')
+    cakeP.innerHTML =  `has ${cake.icing} icing and ${cake.tiers} tiers`
+    cakeElem.append(cakeP)
+    
+    
+    return cakeElem
 }
 
 window.onload = function(){
